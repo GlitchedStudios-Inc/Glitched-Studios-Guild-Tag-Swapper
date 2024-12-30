3b7928499e801c52a6045bc5d64654d4313c998f
Glitched Da Kitty Cat School Account <178820833+GlitchedDaKittyCatSchoolaccount@users.noreply.github.com>
2024-12-30 15:19:52 -0600

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,15 +1,10 @@
 @echo off
 echo Installing required packages
 
-npm install discord.js-selfbot-v13
+call npm install discord.js-selfbot-v13
 
-npm install axios
+call npm install axios
 
-if exist Start.bat (
-    echo Starting the bot
-    start Start.bat
-) else (
-    echo Start.bat not found. Please ensure it exists in the same directory.
-)
+del Setup.bat
 
 pause
